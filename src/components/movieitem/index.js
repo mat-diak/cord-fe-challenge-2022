146b60b838344fb45b5d5dcd54f885d5fc341138
mikitaro-27 <matt.diak@gmail.com>
2022-06-14 12:00:19 +0100

    Populates/styles movieItem for Desktop

--- src/components/movieitem/index.js
+++ src/components/movieitem/index.js
@@ -1,37 +1,111 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import * as colors from '../../colors'
+import * as colors from "../../colors";
+import { getSmImgUrl } from "../../fetcher";
 
-export default function MovieItem ({ movie, genres }) {
+import posterTemplate from "../../images/poster-template.jpeg";
+
+export default function MovieItem({ movie, genres }) {
+  const mappedGenres = genres
+    .filter((genre) => movie.genre_ids.includes(genre.id))
+    .map((genreObj) => genreObj.name);
+
   return (
     // TODO: Complete the MovieItem component
     <MovieItemWrapper>
       <LeftCont>
+        <PosterImage
+          src={getSmImgUrl(movie.poster_path)}
+          alt={"Poster of" + movie.title}
+          onError={(e) => {
+            e.target.onerror = null;
+            e.target.src = posterTemplate;
+          }}
+        />
       </LeftCont>
       <RightCont>
-        <Title>{movie.title}</Title>
+        <Header>
+          <div>
+            {/* TITLE */}
+            <Title>{movie.title}</Title>
+            {/* RATING */}
+            <Rating>{movie.vote_average}</Rating>
+          </div>
+          {/* GENRES */}
+          <Genres>{mappedGenres.join(" | ")}</Genres>
+        </Header>
+        {/* OVERVIEW */}
+        <Overview>{movie.overview}</Overview>
+        {/* DATE */}
+        <Date>{movie.release_date}</Date>
       </RightCont>
     </MovieItemWrapper>
-  )
+  );
 }
 
-const MovieItemWrapper = styled.div`
-  position: relative;
+const Rating = styled.div`
+  background-color: ${colors.primaryColor};
+  color: ${colors.white};
+  padding: 0.1em 0.2em;
+  border-radius: 5px;
+`;
+
+const smallText = styled.div`
+  font-size: 14px;
+  color: ${colors.primaryColor};
+`;
+
+const Date = styled(smallText)`
+  font-weight: 300;
+  vertical-align: baseline;
+`;
+
+const Genres = styled(smallText)`
+  font-weight: 700;
+`;
+
+const Overview = styled.p`
+  margin: 0;
+  padding: 0;
+`;
+
+const PosterImage = styled.img`
+  height: 250px;
+`;
+
+const Header = styled.div`
+  div {
+    display: flex;
+    align-items: flex-start;
+    justify-content: space-between;
+  }
+`;
+
+const MovieItemWrapper = styled.article`
+  /* position: relative; */
+  display: flex;
+
+  gap: 20px;
+
   background-color: ${colors.white};
   border-radius: 5px;
   padding: 20px;
   margin: 15px 0;
-`
+`;
 
 const LeftCont = styled.div`
   display: inline-block;
-`
+`;
 
 const RightCont = styled.div`
-  display: inline-block;
-`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
 const Title = styled.h2`
   font-size: 1.4;
+  font-weight: 800;
+  margin: 0 1em 0.3em 0;
 `;
--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import styled from 'styled-components';
+import styled from "styled-components";
 
 import * as colors from "../../colors";
 import * as fetcher from "../../fetcher";
@@ -8,11 +8,11 @@ import SearchFilters from "../../components/searchfilter";
 import MovieList from "../../components/movielist";
 
 export default class Discover extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
-      keyword: '',
+      keyword: "",
       year: 0,
       results: [],
       totalCount: 0,
@@ -23,75 +23,82 @@ export default class Discover extends React.Component {
         { id: 8.5, name: 8.5 },
         { id: 9, name: 9 },
         { id: 9.5, name: 9.5 },
-        { id: 10, name: 10 }
+        { id: 10, name: 10 },
       ],
       languageOptions: [
-        { id: 'GR', name: 'Greek' },
-        { id: 'EN', name: 'English' },
-        { id: 'RU', name: 'Russian' },
-        { id: 'PO', name: 'Polish' }
-      ]
+        { id: "GR", name: "Greek" },
+        { id: "EN", name: "English" },
+        { id: "RU", name: "Russian" },
+        { id: "PO", name: "Polish" },
+      ],
     };
   }
 
   // TODO: Preload and set the popular movies and movie genres when page loads
-  async componentDidMount () {
+  async componentDidMount() {
     const popularMoviesData = await fetcher.getPopularMovies();
-    const getGenres = await fetcher.getGenres();
-    const byQuery = await fetcher.getMoviesByQuery('Hello')
+    const genresData = await fetcher.getGenres();
 
-    console.log('popularMoviesData', popularMoviesData)
-    console.log('getGenres', getGenres)
-    console.log('byQuery', byQuery)
+    this.setState({
+      results: popularMoviesData.results,
+      totalCount: popularMoviesData.total_results,
+      genreOptions: genresData.genres,
+    });
+    console.log(this.state);
   }
 
   // TODO: Update search results based on the keyword and year inputs
+  // const byQuery = await fetcher.getMoviesByQuery('Hello')
 
-  render () {
-    const { genreOptions, languageOptions, ratingOptions, totalCount, results } = this.state;
+  render() {
+    const {
+      genreOptions,
+      languageOptions,
+      ratingOptions,
+      totalCount,
+      results,
+    } = this.state;
 
     return (
       <DiscoverWrapper>
-        <MobilePageTitle>Discover</MobilePageTitle> {/* MobilePageTitle should become visible on mobile devices via CSS media queries*/}
+        <MobilePageTitle>Discover</MobilePageTitle>{" "}
+        {/* MobilePageTitle should become visible on mobile devices via CSS media queries*/}
         <TotalCount>{totalCount} results</TotalCount>
         <MovieFilters>
-          <SearchFilters 
-            genres={genreOptions} 
-            ratings={ratingOptions}  
+          <SearchFilters
+            genres={genreOptions}
+            ratings={ratingOptions}
             languages={languageOptions}
             searchMovies={(keyword, year) => this.searchMovies(keyword, year)}
           />
         </MovieFilters>
         <MovieResults>
-          <MovieList 
-            movies={results || []}
-            genres={genreOptions || []}
-          />
+          <MovieList movies={results || []} genres={genreOptions || []} />
         </MovieResults>
       </DiscoverWrapper>
-    )
+    );
   }
 }
 
 const DiscoverWrapper = styled.main`
   padding: 35px;
-`
+`;
 
 const MovieResults = styled.div`
   display: inline-block;
   width: calc(100% - 295px);
-`
+`;
 
 const MovieFilters = styled.div`
   width: 280px;
   float: right;
   margin-top: 15px;
-`
+`;
 
 const MobilePageTitle = styled.h1`
   display: none;
-`
+`;
 
 const TotalCount = styled.strong`
   display: block;
-`
+`;
